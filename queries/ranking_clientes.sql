b2ff36b0b78daa52f18c57a03ed68c0614b2589a
Raniel2 <125783658+Raniel2@users.noreply.github.com>
2026-06-18 12:22:04 -0300

    Add SQL query to rank clients by total spending

--- queries/ranking_clientes.sql
+++ queries/ranking_clientes.sql
@@ -1,1 +1,13 @@
-
+SELECT
+    c.id_cliente,
+    c.nome,
+    SUM(it.quantidade * it.preco_unitario) AS total_gasto,
+    RANK() OVER(
+        ORDER BY SUM(it.quantidade * it.preco_unitario) DESC
+    ) AS ranking
+FROM clientes c
+JOIN pedidos p
+    ON c.id_cliente = p.id_cliente
+JOIN itens_pedido it
+    ON p.id_pedido = it.id_pedido
+GROUP BY c.id_cliente,c.nome;
