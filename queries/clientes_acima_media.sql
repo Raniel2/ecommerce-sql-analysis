b9ec9c7e1de07697cced98147af4dfc5ccb7011e
Raniel2 <125783658+Raniel2@users.noreply.github.com>
2026-06-24 12:04:45 -0300

    Add SQL query to categorize clients by spending

--- queries/clientes_acima_media.sql
+++ queries/clientes_acima_media.sql
@@ -1,1 +1,28 @@
+with gasto_cliente as (
+select 
+	c.id_cliente,
+    c.nome,
+    case
+		when  sum(it.quantidade * it.preco_unitario) >= 10000 then "OURO"
+        when  sum(it.quantidade * it.preco_unitario) >= 5000  then "PRATA"
+        else "FERRO"
+    end as categoria,    
+    sum(it.quantidade * it.preco_unitario) as total_gasto
+FROM clientes c
+JOIN pedidos p
+	ON c.id_cliente = p.id_cliente
+JOIN itens_pedido it
+	ON p.id_pedido = it.id_pedido
+GROUP BY c.id_cliente, c.nome),
 
+cliente_media as (
+	select
+        nome,
+        categoria,
+        total_gasto,
+        round(avg(total_gasto) over(partition by categoria),2) as media_gasto
+from gasto_cliente
+)
+select * 
+from cliente_media
+where total_gasto > media_gasto; 
