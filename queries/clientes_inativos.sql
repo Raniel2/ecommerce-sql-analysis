98e1c86dfd23054bed33d807be8482da5a5b7a83
Raniel2 <125783658+Raniel2@users.noreply.github.com>
2026-06-24 11:47:24 -0300

    Add query to retrieve inactive clients

--- queries/clientes_inativos.sql
+++ queries/clientes_inativos.sql
@@ -1,1 +1,10 @@
-
+select
+	c.id_cliente,
+    c.nome,
+    max(p.data_pedido) as ultimo_pedido,
+    datediff(curdate(), MAX(P.data_pedido)) as dias_sem_comprar
+FROM clientes c
+join pedidos p
+	on c.id_cliente = p.id_cliente
+group by id_cliente, nome
+order by dias_sem_comprar desc;
