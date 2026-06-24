f11c120719fd8b9b007250fcfe462f94f2336425
Raniel2 <125783658+Raniel2@users.noreply.github.com>
2026-06-24 11:32:47 -0300

    Add SQL query for average ticket per customer

--- queries/ticket_medio.sql
+++ queries/ticket_medio.sql
@@ -1,1 +1,11 @@
-
+SELECT
+	c.id_cliente,
+    c.nome,
+    round(AVG(it.quantidade * it.preco_unitario),2) AS ticket_medio
+FROM clientes c
+JOIN pedidos p
+	ON c.id_cliente = p.id_cliente
+JOIN itens_pedido it
+	ON p.id_pedido = it.id_pedido
+GROUP BY c.id_cliente, c.nome
+ORDER BY ticket_medio DESC;
