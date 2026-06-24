1a06c53c72550ecab97268afdd3ccf960d7b95ca
Raniel2 <125783658+Raniel2@users.noreply.github.com>
2026-06-24 12:26:43 -0300

    Update create_tables.sql

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,1 +1,66 @@
+-- criaçao do banco de dados e tabelas
 
+CREATE DATABASE ecommerce;
+USE ecommerce;
+
+-- CLIENTES
+CREATE TABLE clientes (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100),
+    cidade VARCHAR(50),
+    data_cadastro DATE
+);
+
+-- CATEGORIAS
+CREATE TABLE categorias (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    nome_categoria VARCHAR(100)
+);
+
+-- PRODUTOS
+CREATE TABLE produtos (
+    id_produto INT AUTO_INCREMENT PRIMARY KEY,
+    nome_produto VARCHAR(100),
+    preco DECIMAL(10,2),
+    id_categoria INT,
+    FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria)
+);
+
+-- PEDIDOS
+CREATE TABLE pedidos (
+    id_pedido INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT,
+    data_pedido DATE,
+    status VARCHAR(50),
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
+);
+
+-- ITENS DO PEDIDO
+CREATE TABLE itens_pedido (
+    id_item INT AUTO_INCREMENT PRIMARY KEY,
+    id_pedido INT,
+    id_produto INT,
+    quantidade INT,
+    preco_unitario DECIMAL(10,2),
+    FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido),
+    FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
+);
+
+-- PAGAMENTOS
+CREATE TABLE pagamentos (
+    id_pagamento INT AUTO_INCREMENT PRIMARY KEY,
+    id_pedido INT,
+    metodo VARCHAR(50),
+    valor DECIMAL(10,2),
+    FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido)
+);
+
+-- AVALIAÇÕES
+CREATE TABLE avaliacoes (
+    id_avaliacao INT AUTO_INCREMENT PRIMARY KEY,
+    id_produto INT,
+    nota INT,
+    comentario TEXT,
+    FOREIGN KEY (id_produto) REFERENCES produtos(id_produto)
+);
