1bf368f75a644f19b530905e04548766975eb665
Raniel2 <125783658+Raniel2@users.noreply.github.com>
2026-07-01 11:33:13 -0300

    Add SQL query for top-selling products

--- queries/produtos_mais_vendidos.sql
+++ queries/produtos_mais_vendidos.sql
@@ -1,1 +1,8 @@
-
+select 
+    p.nome_produto as produto,
+    sum(it.quantidade) as quantidade_vendida
+from produtos p
+join itens_pedido it
+	on p.id_produto = it.id_produto
+group by p.id_produto, produto
+order by quantidade_vendida desc;
