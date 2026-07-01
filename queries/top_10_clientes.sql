a3540e6b06cbd64ba40a2db8c3982290e43d8ec7
Raniel2 <125783658+Raniel2@users.noreply.github.com>
2026-07-01 11:19:29 -0300

    Add SQL query to retrieve top 10 clients by spending

--- queries/top_10_clientes.sql
+++ queries/top_10_clientes.sql
@@ -1,1 +1,12 @@
-
+select
+	c.id_cliente,
+    c.nome,
+    sum(it.quantidade * it.preco_unitario) as total_gasto
+from clientes c
+join pedidos p
+	on c.id_cliente = p.id_cliente
+join itens_pedido it
+	on p.id_pedido = it.id_pedido
+group by c.id_cliente,c.nome
+order by total_gasto desc
+limit 10;
