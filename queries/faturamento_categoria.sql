a10e0149e24f4e97b23b2cdbbba251cdaf5c0f74
Raniel2 <125783658+Raniel2@users.noreply.github.com>
2026-07-01 11:27:42 -0300

    Add SQL query to calculate category revenue

--- queries/faturamento_categoria.sql
+++ queries/faturamento_categoria.sql
@@ -1,1 +1,10 @@
-
+select
+	ct.nome_categoria,
+    sum(it.quantidade * it.preco_unitario) as faturamento
+from categorias ct
+join produtos pr
+	on ct.id_categoria = pr.id_categoria
+join itens_pedido it
+	on pr.id_produto = it.id_produto
+group by ct.nome_categoria
+order by faturamento desc;
